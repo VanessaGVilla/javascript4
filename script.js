6cab3a23f4d413e99ded9fd6344ca47bad9826f8
Vanessa Garcia <112432406+VanessaGVilla@users.noreply.github.com>
2022-11-15 21:25:56 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // 1. Write a function min that takes two numbers and returns the smallest one of the pair.
 // Examples:
+console.log(Math.min(10,6,));
+// expected output: 6
+
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
 
@@ -7,7 +10,18 @@
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
 
+function calculateCircumference(radius) {
+    return 2 * Math.PI * radius;
+  }
+ 
+  console.log(Math.PI);
+  // expected output: 3.14159265358979
+
+  console.log(calculateCircumference(5));
+// expected output: 31.41592653589793  
+
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
 // console.log(searchString(“q”, “mississippi”)); // will output 0
+
